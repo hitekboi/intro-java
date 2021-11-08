76b21207ee6593cc4479fa88ccc30d95e13562f2
hitekboi <79547488+hitekboi@users.noreply.github.com>
2021-11-08 17:58:39 +0100

    My first Commit. Made HelloWorld.java

---
+++ src/space/harbour/helloworld/HelloWorld.java
@@ -0,0 +1,8 @@
+package space.harbour.helloworld;
+//it's a package from space.harbour.helloworld
+
+public class HelloWorld {
+    public static void main(String[] args){
+        System.out.println("Hello World!");
+    }
+}
